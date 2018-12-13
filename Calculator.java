74e47dfcf6969de2f8d776e965e0ec78bb268417
Andrei Barbulescu <andreibarbulescu17@gmail.com>
2018-12-12 19:35:32 -0500

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,22 @@
 public class Calculator
 {
+    int number = 0;
+    public static void main(String[] args)
+    {
+        if (true)
+        {
+            System.out.println(Operations.addition());
+        }
+    }
+
+    public static class Operations
+    {
+        static double result;
+
+        public  double addition(int number)
+        {
+            return result;
+        }
+    }
 
 }
